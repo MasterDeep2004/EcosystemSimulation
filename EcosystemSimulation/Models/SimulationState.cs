4ca431f911a6908180950e000732d8f5f3c6c155
MasterDeep2004 <132770790+MasterDeep2004@users.noreply.github.com>
2026-08-02 02:39:06 +0530

    Update SimulationState.cs

--- EcosystemSimulation/Models/SimulationState.cs
+++ EcosystemSimulation/Models/SimulationState.cs
@@ -1,50 +1,41 @@
-﻿namespace EcosystemSimulation.Models
+public class SimulationState
 {
-    public class SimulationState
+    public int Id { get; set; }
+
+    public int Plants { get; set; }
+
+    public int Herbivores { get; set; }
+
+    public int Carnivores { get; set; }
+
+    public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+
+    public int PlantGrowth { get; set; }
+    public int PlantConsumed { get; set; }
+
+    public int HerbivoreBirths { get; set; }
+    public int HerbivoreDeaths { get; set; }
+
+    public int CarnivoreBirths { get; set; }
+    public int CarnivoreDeaths { get; set; }
+
+    // Useful metric
+    public int TotalPopulation =>
+        Plants + Herbivores + Carnivores;
+
+    public double Fitness()
     {
-        public int Id { get; set; }
+        double balance =
+            1.0 /
+            (1 +
+             Math.Abs(Plants - Herbivores) +
+             Math.Abs(Herbivores - Carnivores));
 
-        // Ecosystem populations
-        public int Plants { get; set; }
-        public int Herbivores { get; set; }
-        public int Carnivores { get; set; }
+        return TotalPopulation * balance;
+    }
 
-        public DateTime Timestamp { get; set; }
-
-        // Detailed tracking (optional, useful for logging)
-        public int PlantGrowth { get; set; }
-        public int PlantConsumed { get; set; }
-        public int HerbivoreBirths { get; set; }
-        public int HerbivoreDeaths { get; set; }
-        public int CarnivoreBirths { get; set; }
-        public int CarnivoreDeaths { get; set; }
-
-        // Fitness score for GA
-        public double Fitness()
-        {
-            // Balance of ecosystem: penalize extremes
-            double balanceScore = 1.0 / (1 + Math.Abs(Plants - Herbivores) + Math.Abs(Herbivores - Carnivores));
-            double totalPopulation = Plants + Herbivores + Carnivores;
-            return totalPopulation * balanceScore;
-        }
-
-        // Optional: simple clone for GA operations
-        public SimulationState Clone()
-        {
-            return new SimulationState
-            {
-                Id = this.Id,
-                Plants = this.Plants,
-                Herbivores = this.Herbivores,
-                Carnivores = this.Carnivores,
-                Timestamp = this.Timestamp,
-                PlantGrowth = this.PlantGrowth,
-                PlantConsumed = this.PlantConsumed,
-                HerbivoreBirths = this.HerbivoreBirths,
-                HerbivoreDeaths = this.HerbivoreDeaths,
-                CarnivoreBirths = this.CarnivoreBirths,
-                CarnivoreDeaths = this.CarnivoreDeaths
-            };
-        }
+    public SimulationState Clone()
+    {
+        return (SimulationState)MemberwiseClone();
     }
 }
