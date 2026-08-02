c4e9ed307390314792177edfd15ba7578b9c40da
MasterDeep2004 <132770790+MasterDeep2004@users.noreply.github.com>
2026-08-02 09:17:04 +0530

    Create SimulationResponse.cs

---
+++ EcosystemSimulation/DTOs/SimulationResponse.cs
@@ -0,0 +1,12 @@
+public class SimulationResponse
+{
+    public int Plants { get; set; }
+
+    public int Herbivores { get; set; }
+
+    public int Carnivores { get; set; }
+
+    public double Fitness { get; set; }
+
+    public DateTime Timestamp { get; set; }
+}
