55b4392ac617d4029b8f74ec2f8e22f8e37eaf09
MasterDeep2004 <132770790+MasterDeep2004@users.noreply.github.com>
2026-08-10 22:43:36 +0530

    Enhance AppDbContext with entity configurations
    Refactor AppDbContext constructor and add OnModelCreating method for entity configurations.

--- EcosystemSimulation/Data/AppDbContext.cs
+++ EcosystemSimulation/Data/AppDbContext.cs
@@ -1,15 +1,32 @@
-using Microsoft.EntityFrameworkCore;
 using EcosystemSimulation.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace EcosystemSimulation.Data
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options)
+        public AppDbContext(
+            DbContextOptions<AppDbContext> options)
             : base(options)
         {
         }
 
+        public DbSet<Entity> Entities { get; set; }
+
         public DbSet<SimulationState> SimulationStates { get; set; }
+
+        protected override void OnModelCreating(
+            ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Entity>()
+                .Property(e => e.Type)
+                .HasConversion<string>();
+
+            modelBuilder.Entity<SimulationState>()
+                .Property(s => s.EventName)
+                .HasMaxLength(100);
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
